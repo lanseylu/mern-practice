d6bb81f4af51a5885bd625798a243ea86714d0f0
lanseylu <100890425+lanseylu@users.noreply.github.com>
2024-09-10 18:08:55 +0800

    Moved files

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:1234@cluster0.29c8r.mongodb.net/bookstore-mern-collection?retryWrites=true&w=majority&appName=Cluster0'
---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+//  Middleware for handling CORS POLICY
+//  Option 1: Allow all origins with default of cors(*)
+// app.use(cors()); 
+
+//  Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack!');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("FAILED TO CONNECT", error);
+    });
+
